8c70ebaf726b91257f582262e14d4eaa927046c1
Henrique <henriquemiranda47@gmail.com>
2020-08-12 13:06:23 -0300

    Added basic scraping

--- Main.py
+++ Main.py
@@ -5,8 +5,7 @@ class Program(scrapy.Spider):
 
     name = 'aranha'
     start_urls = [  #Metodo simplificado de usar a aranha, define somente a lista com o nome start_urls e nao precisa usar o loop for
-        'http://quotes.toscrape.com/tag/humor/',
-        'https://docs.scrapy.org/en/latest/intro/tutorial.html'
+        'http://books.toscrape.com/'
     ]
 
     def main():
@@ -18,8 +17,12 @@ class Program(scrapy.Spider):
     def parse(self, response):
         page = response.url
         print("------------------------------")
-        print("Nome do site: "+ page)
-        print("Nome do site: "+ page.split("//")[1])
+        print(response.css("a.title").getall())
+        print(response.css("p.price_color::text").getall())
+        #print(response.css("p.instock availability::text").getall())
+
+        #print("Nome do site: "+ page)
+        #print("Nome do site: "+ page.split("//")[1])
         print("------------------------------")
 
     if __name__ == "__main__":
