4453de68f2c7900bbf4bf289d149596766e3bd6d
Henrique <henriquemiranda47@gmail.com>
2020-08-14 11:11:31 -0300

    Added second level scrap

--- Spyders/AranhaIndustrial.py
+++ Spyders/AranhaIndustrial.py
@@ -12,22 +12,47 @@ class AranhaIndustrial(scrapy.Spider):
         'https://www.cadastroindustrialmg.com.br:449/industria/setor'
     ]
     
-    #Metido de retorno da request da aranha
-    def retornoRequest(self, response):
+    #Metodo para desbloquear a thread
+    def desligarAranha(self):
+        reactor.stop()
+    
+    #Metodo de retorno da request da aranha no segundo nivel
+    def menuSegundoNivel(self, response):
+        print("MENU SEGUNDO NIVEL")
+        print(response.url)
+        self.desligarAranha()
+
+
+    #Metodo de retorno da request da aranha no primeiro nivel
+    def menuPrimeiroNivel(self, response):
+        subcategorias = []
         resp_dict = json.loads(response.body)
         retorno = resp_dict
         lista = retorno[0].get('subCategoria')
-        print(lista)
         print("----------------------------------------------")
-        #print(lista[0])
-        #print(lista[0].get('Nome'))
         for nomes in lista:
-            print(nomes.get('Nome'))
+            subcategorias.append(nomes.get('Nome'))
+        print(subcategorias)
+        print(subcategorias[0])
+        print(subcategorias[1])
+        print(subcategorias[2])
+        print("----------------------------------------------")
+        print(subcategorias[0].split(' ', 1)[0])
+        print(subcategorias[1].split(' ', 1)[0])
+        print(subcategorias[2].split(' ', 1)[0])
+        
+        ## Montar novo request
+        urlSegundoNivel = "https://www.cadastroindustrialmg.com.br:449/industria/resultadobusca?Filters=Setor:" + subcategorias[0] + ";&K="+  subcategorias[2].split(' ', 1)[0]
+        #print(urlSegundoNivel)
+        next_page = response.urljoin(urlSegundoNivel)
+        yield scrapy.Request(next_page, callback=self.menuSegundoNivel) #Faz um novo request para a aranha 
 
-        #resp_dict['value']['queryInfo']['creationTime'] # 1349724919000
-        reactor.stop()
+        #yield scrapy.Request(self.start_urls[0], callback=self.menuSegundoNivel) #Faz um novo request para a aranha 
+        #yield scrapy.Request(self.start_urls[0], callback=self.parse) #Faz um novo request para a aranha 
 
-    #Metodi inical da aranha sera chado primeiro
+        #self.desligarAranha()
+
+    #Metodo inical da aranha sera chado primeiro
     def start_requests(self):
         url = "https://www.cadastroindustrialmg.com.br:449/Industria/Setor"
         headers= {'Accept':'*/*'} 
@@ -35,12 +60,13 @@ class AranhaIndustrial(scrapy.Spider):
         yield scrapy.FormRequest(url, 
                         formdata = payload,
                         method='POST',
-                        callback = self.retornoRequest)
+                        callback = self.menuPrimeiroNivel)
     
   
-    #def parse(self, response):
-    #    page = response.url
-    #    print("------------%s trabalhando------------------" % self.name)
+    def parse(self, response):
+        #page = response.url
+        print("------------%s trabalhando------------------" % self.name)
+        self.desligarAranha()
     #    print(response)
     #    url = "https://www.cadastroindustrialmg.com.br:449/Industria/Setor"
     #    headers= {'Accept':'*/*'} 
