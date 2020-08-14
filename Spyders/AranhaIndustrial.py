e9b911c030b04e79ec6c6740c6a5c304d6c548c7
Henrique <henriquemiranda47@gmail.com>
2020-08-14 10:29:18 -0300

    Added return method for the spider

--- Spyders/AranhaIndustrial.py
+++ Spyders/AranhaIndustrial.py
@@ -2,8 +2,9 @@
 import scrapy
 from Moeda import Moeda
 from twisted.internet import reactor
+import json
 
-
+#Aranha que vai fazer o scrapping no site do cadastro industrial
 class AranhaIndustrial(scrapy.Spider):
 
     name = 'aranha_industrial'
@@ -11,17 +12,36 @@ class AranhaIndustrial(scrapy.Spider):
         'https://www.cadastroindustrialmg.com.br:449/industria/setor'
     ]
     
+    #Metido de retorno da request da aranha
+    def retornoRequest(self, response):
+        resp_dict = json.loads(response.body)
+        retorno = resp_dict
+        lista = retorno[0].get('subCategoria')
+        print(lista)
+        print("----------------------------------------------")
+        #print(lista[0])
+        #print(lista[0].get('Nome'))
+        for nomes in lista:
+            print(nomes.get('Nome'))
+
+        #resp_dict['value']['queryInfo']['creationTime'] # 1349724919000
+        reactor.stop()
+
+    #Metodi inical da aranha sera chado primeiro
     def start_requests(self):
         url = "https://www.cadastroindustrialmg.com.br:449/Industria/Setor"
         headers= {'Accept':'*/*'} 
         payload = {"id": "24"}
-        return [scrapy.FormRequest(url, 
-                        formdata = payload)]
+        yield scrapy.FormRequest(url, 
+                        formdata = payload,
+                        method='POST',
+                        callback = self.retornoRequest)
     
   
     #def parse(self, response):
     #    page = response.url
     #    print("------------%s trabalhando------------------" % self.name)
+    #    print(response)
     #    url = "https://www.cadastroindustrialmg.com.br:449/Industria/Setor"
     #    headers= {'Accept':'*/*'} 
     #    payload = {"id": "24"}
@@ -32,7 +52,7 @@ class AranhaIndustrial(scrapy.Spider):
         #setores = response.css("div.setor-nome p::text").getall()
         #for i in range(len(setores)):
         #    print("%s " % setores[i])
-        #print("------------------------------")
+       
         
         #next_page = response.css("li.next a::attr(href)").get()  #Pega a url da proxima pagina
         #if next_page is not None:
